c2d61d512e1f8fbe4a224693b37f941cac2f3293
toreauami <mirandayuwi@gmail.com>
2017-07-24 12:29:27 +0300

    modified js file

--- jump.js
+++ jump.js
@@ -1,32 +1,41 @@
  var spriteXpos=500;
 var spriteYpos=350;
-var speedX= 3;
-var speedX2= 2;
-var speedX3=4;
+var speedX= 6;
+var speedX2= 4;
+var speedX3=8;
 var spriteXpos3 = 80;
 var spriteXpos2 = spriteXpos- 80;
 var spritedist;
-var sprite2YPos=20;
+var sprite2YPos=300;
+var sprite2XPos=0;
 function setup (){
-  createCanvas(1000,850);
+  createCanvas(1000,600);
   bg = loadImage("Images/jumpwall.png");
   sprite1=loadImage("Images/brickSprite.png")
   sprite2 =loadImage("Images/jumperrh.png.png")
 }
 function draw(){
   background(bg);
-  image(sprite1,spriteXpos,spriteYpos,100,210);
-  image(sprite2,0,sprite2YPos,50,200);
+  image(sprite1,spriteXpos,spriteYpos,50,100);
+  image(sprite2,sprite2XPos,sprite2YPos,50,150);
     if(keyIsDown(UP_ARROW)){
       sprite2YPos -=5;
+      if(sprite2YPos==150){
+        sprite2YPos=300;
+      }
     }
-    if(keyIsDown(DOWN_ARROW)){
-      sprite2YPos +=5;
-    }
-
-  image(sprite1,spriteXpos,spriteYpos,100,210);
-    image(sprite1,spriteXpos2,spriteYpos,100,210);
-    image(sprite1,spriteXpos3,spriteYpos,100,210);
+    if(keyIsDown(RIGHT_ARROW)){
+      sprite2XPos +=5;
+}
+if(keyIsDown(LEFT_ARROW)){
+  sprite2XPos -=5;
+}
+if(keyIsDown(DOWN_ARROW)){
+  sprite2YPos +=5;
+}
+  image(sprite1,spriteXpos,spriteYpos,50,100);
+    image(sprite1,spriteXpos2,spriteYpos,50,100);
+    image(sprite1,spriteXpos3,spriteYpos,50,100);
 
     spriteXpos3 =spriteXpos3 -speedX3;
    spriteXpos2 =spriteXpos2 -speedX;
