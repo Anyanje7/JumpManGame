45e4509646640bdd653976cf48983bfdf9c1c51d
anyanje7 <johnanyanje@gmail.com>
2017-07-28 10:35:28 +0300

    I have made changes

--- jump.js
+++ jump.js
@@ -8,6 +8,9 @@ var spriteXpos2 = spriteXpos- 80;
 var spritedist;
 var sprite2YPos=300;
 var sprite2XPos=0;
+var myScore;
+var failDist;
+var life=3;
 //var upspeed = 5;
 var score=0;
 function setup (){
@@ -17,25 +20,23 @@ function setup (){
   sprite2 =loadImage("Images/jumperrh.png.png")
 }
 function draw(){
+  clear();
   background(bg);
+
   image(sprite1,spriteXpos,spriteYpos,50,50);
   image(sprite2,sprite2XPos,sprite2YPos,50,100);
 
     if(keyIsDown(UP_ARROW) ){
       sprite2YPos -=100;
+      if(sprite2YPos == 0){
+        sprite2YPos +=100;
+      }
 
     }
 
 //}
-    if(keyIsDown(RIGHT_ARROW)){
-      sprite2XPos +=5;
-}
-if(keyIsDown(LEFT_ARROW)){
-  sprite2XPos -=5;
-}
-if(keyIsDown(DOWN_ARROW)){
-  sprite2YPos +=5;
-}
+
+
   image(sprite1,spriteXpos,spriteYpos,50,50);
     image(sprite1,spriteXpos2,spriteYpos,50,50);
     image(sprite1,spriteXpos3,spriteYpos,50,50);
@@ -61,8 +62,31 @@ if (frameCount % 3 == 0){
  if(sprite2YPos <=200){
       sprite2YPos +=100 ;
     }
-}
-console.log(sprite2YPos);
-//text("score" 20,580);
 
+
+  }
+
+textSize(30);
+fill(255,255,255);
+text("score: " + score, 830,60);
+text("lifes: " + life, 830,90);
+  if (frameCount % 40 == 0){
+    score++;
+  }
+  if (frameCount % 15 == 0){
+
+    if(failDist<=100) {
+      life=life-1;
+
+  }
+}
+  failDist=dist (sprite2XPos,sprite2YPos,spriteXpos,spriteYpos);
+
+
+  if(life<=0){
+    life=life;
+    clear();
+    background(255,0,0);
+    score=score;
+  }
 }
